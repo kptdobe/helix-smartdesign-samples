8d09bc5633abec483327393b62a899134d8e9831
Alexandre Capt <acapt@adobe.com>
2018-09-13 16:52:18 +0200

    Initial commit

--- index.md
+++ index.md
@@ -20,15 +20,46 @@
 
 It works! test is up and running.
 
-## What's next?
+---
+
+Here is an example of rendering when the text is on the left and the image is on the right: ![1](https://images.unsplash.com/photo-1536679887050-4bdea5bf41c6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e2382881d2f2d7346c32da6fca7cf268&auto=format&fit=crop&w=2667&q=80)
+
+---
+
+![4](https://images.unsplash.com/photo-1536668104859-d6a4f9e8bba9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=56632f8d3984097dd123f4cf7e64568f&auto=format&fit=crop&w=1258&q=80) Image is on the left and text on its right in this case!
+
+---
+
+A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... 
+A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there....
+A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... A lot of text, not really usefull but it is there.... but still, text is *justify*!
+
+---
+
+Tiny text
+
+---
+
+![1](https://images.unsplash.com/photo-1536679887050-4bdea5bf41c6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e2382881d2f2d7346c32da6fca7cf268&auto=format&fit=crop&w=2667&q=80)
+![2](https://images.unsplash.com/photo-1536675572774-1b66ac2e26e9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ad74480de1f0442badf7354b0c44cab0&auto=format&fit=crop&w=1271&q=80)
+![3](https://images.unsplash.com/photo-1536582580171-235afe0902cd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=18560784001bed6bedd229c36840fae4&auto=format&fit=crop&w=934&q=80)
+![4](https://images.unsplash.com/photo-1536668104859-d6a4f9e8bba9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=56632f8d3984097dd123f4cf7e64568f&auto=format&fit=crop&w=1258&q=80)
+![5](https://images.unsplash.com/photo-1536649986370-e623f6c7c1e1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2488b7050e40c4972972bc39abdef810&auto=format&fit=crop&w=3334&q=80)
+![6](https://images.unsplash.com/photo-1536641480196-ed8d5b3ed21f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9e32ed3893fed0e0e5a1c5ef59861d3a&auto=format&fit=crop&w=3300&q=80)
+
+--- 
+
+### Some stuff with lists ?
 
 1. Try editing `index.md` and see what happens.
 2. Try editing `src/html.htl` to change the HTML output
 3. Try editing `src/static/style.css` to change the way this page looks
 
-## And then?
+---
 
-4. Commit your changes: `git commit -a`
-5. Publish your project to GitHub: `git add git remote add origin https://github.com/user/repo.git && git push`
-6. Deploy the project: `hlx deploy`
-7. Make it visible to the world: `hlx strains`
+### And then?
+
+1. Commit your changes: `git commit -a`
+2. Publish your project to GitHub: `git add git remote add origin https://github.com/user/repo.git && git push`
+3. Deploy the project: `hlx deploy`
+4. Make it visible to the world: `hlx strains`
--- src/html.pre.js
+++ src/html.pre.js
@@ -15,14 +15,271 @@
  * limitations under the License.
  *
  */
+const select = require('unist-util-select');
+const hastSelect = require('hast-util-select').select
+const toHAST = require('mdast-util-to-hast');
+const toHTML = require('hast-util-to-html');
+const mdastSqueezeParagraphs = require('mdast-squeeze-paragraphs');
+const mdastFlattenImages = require('mdast-flatten-image-paragraphs');
+const mdastFlattenLists = require('mdast-flatten-listitem-paragraphs');
+
+
+/**
+ * The LayoutMachine is an implmentation of a state machine pattern
+ * that tries to intelligently lay out the page.
+ */
+const LayoutMachine = {
+  /*
+    States:
+      init -> hero, flow
+      hero -> flow
+  */
+  validStates: ['hero', 'flow', 'gallery', 'textimage', 'imagetext', 'text'],
+  states: ['init'],
+  get state() {
+    return this.states[this.states.length - 1];
+  },
+  set state(v) {
+    this.states.push(v);
+    return v;
+  },
+  layout: function (section) {
+    // allow manual overide of class
+    // this might be instant-cruft–discuss.
+    if (section.class && section.class.length) {
+      // If class is a valid state, use it, otherwise default to 'flow'
+      if (this.validStates.includes(section.class)) {
+        this.states.push(section.class);
+      } else {
+        this.states.push('flow');
+      }
+    } else {
+      switch (this.state) {
+        case 'init':
+          if (this.isHero(section)) {
+            this.state = 'hero';
+            break;
+          }
+        case 'hero':
+        case 'flow':
+        case 'textimage':
+        case 'imagetext':
+        case 'text':
+          if (this.isTextImage(section)) {
+            this.state = 'textimage';
+          } else {
+            if (this.isImageText(section)) {
+              this.state = 'imagetext';
+            } else {
+              if (this.isText(section)) {
+                this.state = 'text';
+              } else {
+                if (this.isGallery(section)) {
+                  this.state = 'gallery';
+                } else {
+                  this.state = 'flow';
+                }
+              }
+            }
+          }
+          break;
+      }
+      section.class = this.state;
+    }
+
+    let children = [];
+    for (let e of section.children) {
+      children.push(e);
+    }
+    section.children = children;
+    return section;
+  },
+
+  get hasHero() {
+    return this.states.includes('hero');
+  },
+
+  isHero(section) {
+    // If the section has one paragraph and one image, it's a hero
+    const images = select(section, 'image');
+    const paragraphs = select(section, 'paragraph');
+    return (paragraphs.length == 1 && images.length == 1);
+  },
+
+  isTextImage(section) {
+    // If the section start with a paragraph then an image, it's a text image
+    return (section.children.length > 1 && section.children[0].type == 'paragraph' && section.children[1].type == 'image');
+  },
+
+  isImageText(section) {
+    // If the section start with an image then a paragraph, it's a text image
+    return (section.children.length > 1 && section.children[1].type == 'paragraph' && section.children[0].type == 'image');
+  },
+
+  isText(section) {
+    // If the section contains only paragraph and optionally starts with a heading, it's a text
+    const images = select(section, 'image');
+    const paragraphs = select(section, 'paragraph');
+    const headings = select(section, 'heading');
+    return images.length == 0 && paragraphs.length > 0 && (headings.length == 0 || section.children[0].type == 'heading');
+  },
+
+  isGallery(section) {
+    // If the section has more than 2 images, it is a gallery
+    const images = select(section, 'image');
+    const paragraphs = select(section, 'paragraph');
+    return images.length > 2 && paragraphs.length == 0;
+  },
+}
+
+function getSmartDesign(mdast, breakSection) {
+  breakSection = breakSection ? breakSection : function (node) {
+    return node.type == 'thematicBreak';
+  };
+
+  mdast = mdastFlattenImages()(mdast);
+  mdast = mdastFlattenLists()(mdast);
+  mdast = mdastSqueezeParagraphs(mdast);
+
+  const mdastNodes = mdast.children;
+
+  const sections = [];
+  let currentSection = {
+    children: [],
+    type: 'standard'
+  };
+
+  let title;
+
+  mdastNodes.forEach(function (node) {
+    if (node.type == 'heading' && node.depth == 1 && !title) {
+      title = node.children[0].value;
+      return;
+    }
+    const br = breakSection(node);
+    if (br.break) {
+      sections.push(LayoutMachine.layout(currentSection));
+      currentSection = {
+        children: [],
+        type: 'standard'
+      };
+      if (br.include) {
+        currentSection.children.push(node);
+      }
+    } else {
+      currentSection.children.push(node);
+    }
+  });
+
+  sections.push(LayoutMachine.layout(currentSection));
+  return sections;
+}
+
+function computeSectionsHAST(sections) {
+  const nodes = [];
+  let odd = false;
+  sections.forEach(function (section) {
+    const hast = toHAST(section);
+    const htmlNodes = [];
+    hast.children.forEach(function (h) {
+      htmlNodes.push(toHTML(h));
+    });
+    nodes.push({
+      type: "element",
+      properties: {
+        className: section.class + ' ' + ((odd = !odd) ? 'odd' : 'even'),
+      },
+      tagName: 'section',
+      children: hast.children,
+      data: {
+        type: section.class
+      }
+    });
+  });
+  return nodes;
+}
+
+function sectionsPipeline(payload, breakSection) {
+  // get the sections MDAST
+  const sectionsMdast = getSmartDesign(payload.content.mdast, breakSection);
+
+  // get the sections MDAST
+  const sectionsHAST = computeSectionsHAST(sectionsMdast);
+
+  // create a "convienence object" that gives access to individual mdast, hast and html for each section.
+  const sectionsDetails = [];
+
+  sectionsMdast.forEach(function (mdast, index) {
+    const hast = sectionsHAST[index];
+    sectionsDetails.push({
+      mdast: mdast,
+      hast: hast,
+      html: toHTML(hast),
+      type: hast.data.type
+    });
+  });
+
+  // convert full HAST to html
+  const html = toHTML({
+    type: 'root',
+    children: sectionsHAST
+  });
+
+  return {
+    html,
+    children: sectionsDetails
+  }
+}
 
 /**
  * The 'pre' function that is executed before the HTML is rendered
  * @param payload The current payload of processing pipeline
- * @param payload.resource The content resource
+ * @param payload.content The content resource
  */
 function pre(payload) {
-  payload.resource.time = `${new Date()}`;
+
+  // banner is first image and banner text is image alt
+  payload.content.banner = {
+    img: '',
+    text: ''
+  };
+  const firstImg = select(payload.content.mdast, 'image');
+  if (firstImg.length > 0) {
+    payload.content.banner = {
+      img: firstImg[0].url,
+      text: firstImg[0].alt
+    }
+  }
+
+  const determineBreaks = function (mdast) {
+    const isTB = mdast.type == 'thematicBreak'; // ---
+    const isH2 = mdast.type == 'heading' && mdast.depth == 2;
+    return {
+      break: isTB || isH2,
+      include: isH2
+    }
+  }
+  payload.content.sections = sectionsPipeline(payload, determineBreaks);
+
+  // EXTENSION point demo
+  // -> I need a different DOM for the hero section
+  if (payload.content.sections.children.length > 0 && payload.content.sections.children[0].type == 'hero') {
+    const hero = payload.content.sections.children[0].hast;
+    const img = hastSelect('img', hero);
+    const p = hastSelect('p', hero);
+
+    // create object to be consumed in HTML to render custom HTML for hero section
+    payload.content.sections.hero = {
+      sectionClass: hero.properties.className,
+      img: toHTML(img),
+      p: toHTML(p)
+    };
+  }
+
+  // avoid htl execution error if missing
+  payload.content.meta = payload.content.meta || {};
+  payload.content.meta.references = payload.content.meta.references || [];
+  payload.content.meta.icon = payload.content.meta.icon || '';
 }
 
 module.exports.pre = pre;
